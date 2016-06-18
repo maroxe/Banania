33a040b2a9aae7a51505ed6e20a75ce70da39a51
bachir el khadir <maroxe@bachirs-MacBook-Air.local>
2016-06-18 19:04:02 -0400

    Make AI speed independent of framerate.

--- game/ai.py
+++ game/ai.py
@@ -9,10 +9,14 @@ class UnitAi(object):
 
 class EnemyFollowingTargetAi(object):
 
+    power_follow = 60.
+
     def __init__(self, target):
         self.target = target
 
     def update(self, dt, unit):
-        u = self.target.get_position() - unit.get_position()
+        u = (self.target.get_position() - unit.get_position())
+        u *= dt * self.power_follow
+
         if random.random() > 50*dt:
             unit.move(dt, u)
